39ef6c95cb1e66f36c054bace1ace566021b629b
wooffet <wooffet@gmail.com>
2021-02-07 20:09:14 +0000

    Lecture 13 changes
    Change Navbar and UserItem to function components

--- project-1-github-finder/src/components/layout/Navbar.tsx
+++ project-1-github-finder/src/components/layout/Navbar.tsx
@@ -1,25 +1,19 @@
-import { Component } from 'react';
-
 interface NavbarProps {
-  title: string;
-  icon: string;
+  title?: string;
+  icon?: string;
 }
 
-class Navbar extends Component<NavbarProps> {
-  static defaultProps = {
-    title: 'GitHub Finder',
-    icon: 'fab fa-github',
-  };
-
-  render() {
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>
-          <i className={this.props.icon} /> {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+const Navbar = ({
+  title = 'GitHub Finder',
+  icon = 'fab fa-github',
+}: NavbarProps) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+    </nav>
+  );
+};
 
 export default Navbar;
--- project-1-github-finder/src/components/users/UserItem.tsx
+++ project-1-github-finder/src/components/users/UserItem.tsx
@@ -1,33 +1,25 @@
-import { Component } from 'react';
 import User from './intefaces/User';
-
 interface UserItemProps {
   user: User;
 }
-interface UserItemState {
-  user: User;
-}
 
-class UserItem extends Component<UserItemProps, UserItemState> {
-  render() {
-    const { login, avatarUrl, htmlUrl } = this.props.user;
-    return (
-      <div className='card text-center'>
-        <img
-          src={avatarUrl}
-          alt=''
-          className='round-img'
-          style={{ width: '60px' }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={htmlUrl} className='btn btn-dark btn-sm my-1'>
-            More
-          </a>
-        </div>
+const UserItem = ({ user: { login, avatarUrl, htmlUrl } }: UserItemProps) => {
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatarUrl}
+        alt=''
+        className='round-img'
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={htmlUrl} className='btn btn-dark btn-sm my-1'>
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default UserItem;
