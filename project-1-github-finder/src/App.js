f1e16e5f1a4ae3f305c4104960e6b7a6b6acbcae
wooffet <wooffet@gmail.com>
2021-02-07 13:25:46 +0000

    Lecture 9 changes
    Use expressions and literals in App.js

--- project-1-github-finder/src/App.js
+++ project-1-github-finder/src/App.js
@@ -11,12 +11,18 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const name = 'John Doe';
+    const loading = false;
+    const showName = true;
+
     return (
       // Lecture 8 notes:
       // Either return one element e.g. div, <React.Fragment> or <>
       // Prefer to not use <>
       <div className="App">
-        <h1>Hello from React</h1>
+        {loading ?
+          <h4>Loading...</h4>
+          : <h1>Hello {showName && name}</h1>}        
       </div>
     );
   }  
