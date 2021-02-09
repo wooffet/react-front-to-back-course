e481bc0c00ffaec919a0b98e7df5ff3ff774df5c
wooffet <wooffet@gmail.com>
2021-02-09 12:06:06 +0000

    Lecture 27 changes
    Change UserProfile component to function based
    Add `useEffect` to UserProfile component in place of `componentDidMount`

--- project-1-github-finder/src/components/users/UserProfile.tsx
+++ project-1-github-finder/src/components/users/UserProfile.tsx
@@ -1,4 +1,4 @@
-import { Component, Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import { RouteComponentProps, Link } from 'react-router-dom';
 import User from './intefaces/User';
 import Spinner from '../layout/Spinner';
@@ -17,109 +17,111 @@ interface UserProfileProps {
   loading: boolean;
 }
 
-class UserProfile extends Component<
-  UserProfileProps & RouteComponentProps<UserProfileParams>
-> {
-  componentDidMount() {
-    this.props.getUser(this.props.match.params.login);
-    this.props.getUserRepos(this.props.match.params.login);
+const UserProfile = ({
+  getUser,
+  getUserRepos,
+  user,
+  userRepos,
+  loading,
+  match,
+}: UserProfileProps & RouteComponentProps<UserProfileParams>) => {
+  useEffect(() => {
+    getUser(match.params.login);
+    getUserRepos(match.params.login);
+    // eslint-disable-next-line
+  }, []);
+
+  if (!user) {
+    return null;
   }
 
-  render() {
-    if (!this.props.user) {
-      return null;
-    }
+  const {
+    name,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+    company,
+  } = user as User;
 
-    const {
-      name,
-      avatar_url,
-      location,
-      bio,
-      blog,
-      login,
-      html_url,
-      followers,
-      following,
-      public_repos,
-      public_gists,
-      hireable,
-      company,
-    } = this.props.user;
+  if (loading) {
+    return <Spinner />;
+  }
 
-    const { userRepos, loading } = this.props;
-
-    if (loading) {
-      return <Spinner />;
-    }
-
-    return (
-      <Fragment>
-        <Link to='/' className='btn btn-light'>
-          Back To Search
-        </Link>
-        Hireable:{' '}
-        {hireable ? (
-          <i className='fas fa-check text-success' />
-        ) : (
-          <i className='fas fa-times-circle text-danger' />
-        )}
-        <div className='card grid-2'>
-          <div className='all-center'>
-            <img
-              src={avatar_url}
-              className='round-img'
-              alt='avatar'
-              style={{ width: '150px' }}
-            />
-            <h1>{name}</h1>
-            <p>Location: {location}</p>
-          </div>
-          <div>
-            {bio && (
+  return (
+    <Fragment>
+      <Link to='/' className='btn btn-light'>
+        Back To Search
+      </Link>
+      Hireable:{' '}
+      {hireable ? (
+        <i className='fas fa-check text-success' />
+      ) : (
+        <i className='fas fa-times-circle text-danger' />
+      )}
+      <div className='card grid-2'>
+        <div className='all-center'>
+          <img
+            src={avatar_url}
+            className='round-img'
+            alt='avatar'
+            style={{ width: '150px' }}
+          />
+          <h1>{name}</h1>
+          <p>Location: {location}</p>
+        </div>
+        <div>
+          {bio && (
+            <Fragment>
+              <h3>Bio</h3>
+              <p>{bio}</p>
+            </Fragment>
+          )}
+          <a href={html_url} className='btn btn-dark my-1'>
+            Visit Profile
+          </a>
+          <ul>
+            <li>
               <Fragment>
-                <h3>Bio</h3>
-                <p>{bio}</p>
+                <strong>Username: </strong> {login}
               </Fragment>
-            )}
-            <a href={html_url} className='btn btn-dark my-1'>
-              Visit Profile
-            </a>
-            <ul>
-              <li>
+            </li>
+            <li>
+              {company && (
                 <Fragment>
-                  <strong>Username: </strong> {login}
+                  <strong>Company: </strong> {company}
                 </Fragment>
-              </li>
-              <li>
-                {company && (
-                  <Fragment>
-                    <strong>Company: </strong> {company}
-                  </Fragment>
-                )}
-              </li>
-              <li>
-                {blog && (
-                  <Fragment>
-                    <strong>Website: </strong>{' '}
-                    <a href={blog} style={{ color: 'black' }}>
-                      {blog}
-                    </a>
-                  </Fragment>
-                )}
-              </li>
-            </ul>
-          </div>
+              )}
+            </li>
+            <li>
+              {blog && (
+                <Fragment>
+                  <strong>Website: </strong>{' '}
+                  <a href={blog} style={{ color: 'black' }}>
+                    {blog}
+                  </a>
+                </Fragment>
+              )}
+            </li>
+          </ul>
         </div>
-        <div className='card text-center'>
-          <div className='badge badge-primary'>Followers: {followers}</div>
-          <div className='badge badge-success'>Following: {following}</div>
-          <div className='badge badge-light'>Public Repos: {public_repos}</div>
-          <div className='badge badge-dark'>Public Gists: {public_gists}</div>
-        </div>
-        {userRepos && <Repos userRepos={userRepos} />}
-      </Fragment>
-    );
-  }
-}
+      </div>
+      <div className='card text-center'>
+        <div className='badge badge-primary'>Followers: {followers}</div>
+        <div className='badge badge-success'>Following: {following}</div>
+        <div className='badge badge-light'>Public Repos: {public_repos}</div>
+        <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+      </div>
+      {userRepos && <Repos userRepos={userRepos} />}
+    </Fragment>
+  );
+};
 
 export default UserProfile;
